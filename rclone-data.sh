1ba6d350d5f7833558d33be04b9ab1d39fdd753e
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2023-05-04 14:03:55 -0700

    spring cleaning

--- rclone-data.sh
+++ rclone-data.sh
@@ -2,20 +2,6 @@
 
 echo "Backup started: $(date)" >> ${FOUNDRY_DATA_DIR}/bkp.log
 
-# Check if Foundry is running and if backup has already run at least once
-# if [pgrep -x "node" >/dev/null] && [test -f ${FOUNDRY_DATA_DIR}/bkp.log];
-# then 
-#     echo "Foundry still running :)"
-# else
-#     if [test -f ${FOUNDRY_DATA_DIR}/bkp.log];
-#     then
-#         echo "Foundry stopped, kicking"
-#         node ${FOUNDRY_APP_DIR}/resources/app/main.js --headless --dataPath=${FOUNDRY_DATA_DIR}
-#     else
-#         echo "First time through, touch nothing until backed up"
-#     fi
-# fi
-
 # Check for previous backup and abort if running
 pgrep -x rclone && killall -9 rclone
 
