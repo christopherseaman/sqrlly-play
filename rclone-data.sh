96b4b754489d68e1f96a1f7d7d505e11413d708d
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2023-07-20 07:17:33 -0700

    don't expect this to work

--- rclone-data.sh
+++ rclone-data.sh
@@ -3,6 +3,5 @@
 source ${FOUNDRY_BASE}/dot.env
 
 rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
-        --config=${FOUNDRY_BASE}/rclone.conf --verbose 
-        # \
+        --config=${FOUNDRY_BASE}/rclone.conf --verbose # \
         # --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
