87ebcf534989438c1023f3b43dd8de59ea0295e7
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-07-20 12:10:00 -0700

    GATTACA!

--- rclone-data.sh
+++ rclone-data.sh
@@ -4,4 +4,4 @@ source ${FOUNDRY_BASE}/dot.env
 
 rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
         --config=${FOUNDRY_BASE}/rclone.conf \
-        --stats-one-line --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
+        --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
