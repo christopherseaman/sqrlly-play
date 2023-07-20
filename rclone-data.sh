52a72071db37beabe26440937b8fcf722b6a9970
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2023-07-19 22:59:38 -0700

    console me

--- rclone-data.sh
+++ rclone-data.sh
@@ -3,5 +3,6 @@
 source ${FOUNDRY_BASE}/dot.env
 
 rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
-        --config=${FOUNDRY_BASE}/rclone.conf --verbose \
-        --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
+        --config=${FOUNDRY_BASE}/rclone.conf --verbose 
+        # \
+        # --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
