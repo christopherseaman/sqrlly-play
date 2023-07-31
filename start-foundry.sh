b9e176a26672cb27353495ac90d05d00d990b07e
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-07-31 13:20:28 -0700

    works on sfgp

--- start-foundry.sh
+++ start-foundry.sh
@@ -14,14 +14,35 @@ fi
 
 # Configure rclone
 if [ -f ${FOUNDRY_BASE}/rclone.conf ]
+then
   echo "Existing rclone.conf found"
 else
   echo "Configuring rclone..."
   echo "[dbx]" > ${FOUNDRY_BASE}/rclone.conf
   echo "type = dropbox" >> ${FOUNDRY_BASE}/rclone.conf
   echo "token = ${DROPBOX_TOKEN}" >> ${FOUNDRY_BASE}/rclone.conf
-  echo "rclone config:"
+fi
+
+# Fetch data snapshot
+if [ -f ${FOUNDRY_BASE}/data.zip ] || [ -d ${FOUNDRY_VTT_DATA_PATH} ]
+then
+  echo "Existing snapshot found"
+else
+  echo "Fetching data snapshot..."
+  curl -L ${FOUNDRY_DATA_URL} -o ${FOUNDRY_BASE}/data.zip
+  unzip -q ${FOUNDRY_BASE}/data.zip -d ${FOUNDRY_BASE}
+  rm ${FOUNDRY_BASE}/data.zip
+fi
+if rclone sync dbx:/Shares/foundry/data ${FOUNDRY_VTT_DATA_PATH} \
+          --config=${FOUNDRY_BASE}/rclone.conf \
+          --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
+then
+  echo "Data snapshot fetched successfully."
+else
   cat ${FOUNDRY_BASE}/rclone.conf
+  echo "Environment:"
+  env
+  exit 1
 fi
 
 # Clear backup log if it exists
@@ -29,26 +50,6 @@ if [ -f ${FOUNDRY_VTT_DATA_PATH}/bkp.log ]; then
   echo "Clearing backup log..."
   rm ${FOUNDRY_VTT_DATA_PATH}/bkp.log
 fi
-
-# Fetch data snapshot
-echo "Fetching data snapshot..."
-curl -L ${FOUNDRY_DATA_URL} -o ${FOUNDRY_BASE}/data.zip
-unzip -q ${FOUNDRY_BASE}/data.zip -d ${FOUNDRY_BASE}
-rm ${FOUNDRY_BASE}/data.zip
-if rclone sync dbx:/Shares/foundry/data ${FOUNDRY_VTT_DATA_PATH} \
-          --config=${FOUNDRY_BASE}/rclone.conf \
-          --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log
-then
-  echo "Data snapshot fetched successfully."
-else
-  echo "Data snapshot fetch failed."
-  echo "rclone config:"
-  cat ${FOUNDRY_BASE}/rclone.conf
-  echo "Backup log:"
-  cat ${FOUNDRY_VTT_DATA_PATH}/bkp.log
-  exit 1
-fi
-
 
 # Local settings for Foundry
 echo "Configuring Foundry..."
@@ -63,4 +64,5 @@ pm2 start ${FOUNDRY_MAIN} --name "sqrlly-play"
 
 pm2 start ${FOUNDRY_BASE}/rclone-data.sh -c "${CRONFREQ}" --no-autorestart --name sqrlly-backup
 
-pm2 logs
+# Debug
+# pm2 logs
