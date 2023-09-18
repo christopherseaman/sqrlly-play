b305f4a78e1bb47f344531645c0af68cd63c43ae
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-09-18 16:58:48 -0700

    read the dang dot-file

--- rclone-data.sh
+++ rclone-data.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-if [ -f ${FOUNDRY_BASE}/dot.env ]; then
+if [ -f dot.env ]; then
     while IFS= read -r line; do
         # Skip empty lines
         if [ -z "$line" ]; then
             continue
         fi
         export "$line"
-    done < <(grep -vE '^#|^$' ${FOUNDRY_BASE}/dot.env)
+    done < <(grep -vE '^#|^$' dot.env)
 fi
 
 rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
         --config=${FOUNDRY_BASE}/rclone.conf \
-        --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log \
-        --tpslimit 10 --tpslimit-burst 10 --transfers 5 --dropbox-batch-mode sync
+        --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log 
+        # --tpslimit 10 --tpslimit-burst 10 --transfers 5 --dropbox-batch-mode sync
