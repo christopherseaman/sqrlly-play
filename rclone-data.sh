413059b1c3e7842ff5c4eaadf136d8123f5308c5
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-12-05 17:36:10 -0800

    wrap logging

--- rclone-data.sh
+++ rclone-data.sh
@@ -11,7 +11,12 @@ load_env () {
 DOTFILE=${1:-dot.env}
 load_env ${DOTFILE}
 
+# log start of backup in format "2023/12/05 17:30:27 INFO  : UPDATE"
+echo "$(date +'%Y/%m/%d %H:%M:%S') META  : Starting backup of ${FOUNDRY_VTT_DATA_PATH}" >> ${FOUNDRY_VTT_DATA_PATH}/bkp.log
+
 rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
         --config=${FOUNDRY_BASE}/rclone.conf \
         --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log 
         # --tpslimit 10 --tpslimit-burst 10 --transfers 5 --dropbox-batch-mode sync
+
+echo "$(date +'%Y/%m/%d %H:%M:%S') META  : Finished backup of ${FOUNDRY_VTT_DATA_PATH}" >> ${FOUNDRY_VTT_DATA_PATH}/bkp.log
