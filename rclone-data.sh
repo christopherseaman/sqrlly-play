c22f9169857d467f78b8700a90498875c79692c6
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-12-15 13:46:36 -0800

    better excludes and logging

--- rclone-data.sh
+++ rclone-data.sh
@@ -10,13 +10,17 @@ load_env () {
 # Load variables from first arg, else dot.env if exists
 DOTFILE=${1:-dot.env}
 load_env ${DOTFILE}
+REMOTE="dbx:/Shares/foundry/data"
+OPTIONS="--config=${FOUNDRY_BASE}/rclone.conf -v --stats-one-line --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log"
+EXCLUDE="--exclude=*.log --exclude=*.lock"
 
-# log start of backup in format "2023/12/05 17:30:27 INFO  : UPDATE"
+# Log start of backup
 echo "$(date +'%Y/%m/%d %H:%M:%S') META  : Starting backup of ${FOUNDRY_VTT_DATA_PATH}" >> ${FOUNDRY_VTT_DATA_PATH}/bkp.log
 
-rclone sync ${FOUNDRY_VTT_DATA_PATH} dbx:/Shares/foundry/data \
-        --config=${FOUNDRY_BASE}/rclone.conf \
-        --stats-one-line -v --log-file ${FOUNDRY_VTT_DATA_PATH}/bkp.log 
-        # --tpslimit 10 --tpslimit-burst 10 --transfers 5 --dropbox-batch-mode sync
+# Run rclone sync
+rclone sync ${FOUNDRY_VTT_DATA_PATH} ${REMOTE} ${OPTIONS} ${EXCLUDE}
+rclone copy ${FOUNDRY_VTT_DATA_PATH}/bkp.log ${REMOTE} ${OPTIONS}
 
+# Log end of backup
 echo "$(date +'%Y/%m/%d %H:%M:%S') META  : Finished backup of ${FOUNDRY_VTT_DATA_PATH}" >> ${FOUNDRY_VTT_DATA_PATH}/bkp.log
+echo "" >> ${FOUNDRY_VTT_DATA_PATH}/bkp.log
